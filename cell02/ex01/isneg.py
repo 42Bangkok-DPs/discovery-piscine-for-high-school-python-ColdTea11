deb8743e13cb9d99bc1be2076fb24fa4b3033056
ColdTea11 <tekendo.pp@gmail.com>
2024-10-07 19:44:33 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+Num = input('please put your number ')
+Num = float(Num)
+if Num > 0 :
+    print('This number is positive')
+elif Num < 0 :
+    print('This number is negative')
+elif Num == 0 :
+    print('This Number is equal to zero')
