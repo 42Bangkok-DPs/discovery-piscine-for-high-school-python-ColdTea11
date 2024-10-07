fc23a58ecdd3cf7e69374aa32536c7c1dd1d05ea
ColdTea11 <tekendo.pp@gmail.com>
2024-10-07 19:55:25 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = input('Please enter your password :')
+Password = ('Python is awesome')
+if password != Password :
+    print('ACCESS DEINED')
+elif password == Password :
+    print('ACCESS GRANTED')
