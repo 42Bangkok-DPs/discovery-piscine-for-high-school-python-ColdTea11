406731815520c5f36af6f654fe2e7580ea712ea8
ColdTea11 <tekendo.pp@gmail.com>
2024-10-08 15:31:40 +0700

    Add files via upload

---
+++ cell04/ex00/uppercase_it.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+word = input('Say something? :')
+x = word.upper ()
+print(x)
---
+++ cell04/ex01/age.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+age = input('Please tell me yourr age :')
+age = int(float(age))
+x = age+10
+y = age+20
+z = age+30
+print('You are currently ',age,' years old.')
+print('In 10 years,You will be ',x,' years old')
+print('In 20 years,You will be ',y,' years old')
+print('In 30 years,You will be ',z,' years old')
