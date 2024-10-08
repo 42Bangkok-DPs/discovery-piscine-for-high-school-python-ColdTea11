af4264053f2c8cb99c5c74339c53f092421c80b3
ColdTea11 <tekendo.pp@gmail.com>
2024-10-08 15:30:57 +0700

    Add files via upload

---
+++ cell03/ex02./i_got_that.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+ans = input('What do you want to say?: ')
+while True:
+  if ans == "STOP" :
+    print('okay see ya')
+    break
+  else:
+    ans = input('I got that! anything else: ')
